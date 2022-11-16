85d5e8802fd090f7bcb9a9800485bbeaf4387abf
laralpc <118173508+laralpc@users.noreply.github.com>
2022-11-16 13:40:22 +0100

    Add files via upload

---
+++ ej2.py
@@ -0,0 +1,8 @@
+hi=float(input('Dime tu nota del Hito individual: '))
+hg=float(input('Dime tu nota del Hito Grupal: '))
+ne=float(input('Dime tu nota del examen: '))
+nota1=hi*0.3
+nota2=hg*0.2
+nota3=ne*0.5
+Total=nota1+nota2+nota3
+print(f'Tunota del trimestre es {round(Total)} ')
