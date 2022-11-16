ccdc55e1ef9820b6729ae9adba8c3132c2d9d356
laralpc <118173508+laralpc@users.noreply.github.com>
2022-11-16 13:54:21 +0100

    Add files via upload

---
+++ ej3.py
@@ -0,0 +1,6 @@
+p1=int(input('¿Cuantas preguntas has respondido bien? '))
+p2=int(input('¿Cuantas preguntas has respondido mal? '))
+n1=p1*0.5
+n2=p2*0.25
+nF=n1-n2
+print(f'Tu nota del examen es {nF}')
